9d8b99eeca5fa2dec61d258ffb611561c339d1ad
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-05-06 13:48:12 +0200

    Added test where SUT changes and collaborators are the same

---
+++ tests/OpenMailTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class OpenMailTest extends PHPUnit_Framework_TestCase
+{
+    public function testShowAMailSubjectTextAndSenderAddressWithAMailtoLink()
+    {
+        // $mail = new OpenMail('Urgent', 'Lorem ipsum...', new MailtoAddress('isaac@example.com'));
+        $this->markTestIncomplete();
+    }
+
+    public function testShowAMailSubjectTextAndSenderAddressWithAnInApplicationLink()
+    {
+        // $mail = new OpenMail('Urgent', 'Lorem ipsum...', new NewMessageLinkAddress('isaac@example.com'));
+        $this->markTestIncomplete();
+    }
+
+    public function testShowAMailSubjectTextAndSenderAddressWithNoLink()
+    {
+        // $mail = new OpenMail('Urgent', 'Lorem ipsum...', new PlainTextAddress('isaac@example.com'));
+        $this->markTestIncomplete();
+    }
+}

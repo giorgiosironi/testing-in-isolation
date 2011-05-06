829b39437cb1362bdf20fa059519fda7225ddcdc
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-05-06 14:34:59 +0200

    Added second test: elimination of duplicates

--- tests/AddressBookGeneratorTest.php
+++ tests/AddressBookGeneratorTest.php
@@ -14,6 +14,21 @@ class AddressBookGeneratorTest extends PHPUnit_Framework_TestCase
             '2@example.com'
         ), $generator->getAddresses());
     }
+
+    public function testExtractsAddressesWithoutDuplicates()
+    {
+        $generator = new AddressBookGenerator(3, new MessagesStub(array(
+            new Message('1@example.com', null),
+            new Message('1@example.com', null),
+            new Message('2@example.com', null),
+            new Message('2@example.com', null),
+            new Message('3@example.com', null)
+        )));
+        $this->assertEquals(array(
+            '1@example.com',
+            '2@example.com'
+        ), $generator->getAddresses());
+    }
 }
 
 class MessagesStub implements Messages

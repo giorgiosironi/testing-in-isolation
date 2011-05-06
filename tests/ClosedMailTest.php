b52bd59c60c5f0ac274efb7e98db28f349bdc66e
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-05-06 13:57:47 +0200

    Unified 3 integration tests of ClosedMail into a single unit test

--- tests/ClosedMailTest.php
+++ tests/ClosedMailTest.php
@@ -2,29 +2,17 @@
 
 class ClosedMailTest extends PHPUnit_Framework_TestCase
 {
-    public function testShowAMailSubjectAndSenderAddressWithAMailtoLink()
+    public function testShowAMailSubjectAndASenderAddress()
     {
-        $mail = new ClosedMail('Urgent', new MailtoAddress('isaac@example.com'));
+        $stubAddress = $this->getMock('MailAddress');
+        $stubAddress->expects($this->any())
+                    ->method('__toString')
+                    ->will($this->returnValue('dummyAddress'));
+        $mail = new ClosedMail('Urgent', $stubAddress);
         $expected = "<tr>\n"
                   . "<td class=\"subject\">Urgent</td>\n"
-                  . "<td class=\"address\"><a href=\"mailto:isaac@example.com\">isaac@example.com</a></td>\n"
+                  . "<td class=\"address\">dummyAddress</td>\n"
                   . "</tr>";
         $this->assertEquals($expected, $mail->__toString());
-    }
-
-    public function testShowAMailSubjectAndSenderAddressWithAnInApplicationLink()
-    {
-        $mail = new ClosedMail('Urgent', new NewMessageLinkAddress('isaac@example.com'));
-        $expected = "<tr>\n"
-                  . "<td class=\"subject\">Urgent</td>\n"
-                  . "<td class=\"address\"><a href=\"/new-mail?to=isaac@example.com\">isaac@example.com</a></td>\n"
-                  . "</tr>";
-        $this->assertEquals($expected, $mail->__toString());
-    }
-
-    public function testShowAMailSubjectAndSenderAddressWithNoLink()
-    {
-        // $mail = new ClosedMail('Urgent', new PlainTextAddress('isaac@example.com'));
-        $this->markTestIncomplete();
     }
 }

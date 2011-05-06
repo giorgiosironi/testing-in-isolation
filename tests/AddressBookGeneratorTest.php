bf63e642735b9ce4f7b532474e1abddd71376152
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-05-06 14:25:46 +0200

    Defined contract with collaborator which performs ordering

--- tests/AddressBookGeneratorTest.php
+++ tests/AddressBookGeneratorTest.php
@@ -4,10 +4,24 @@ class AddressBookGeneratorTest extends PHPUnit_Framework_TestCase
 {
     public function testExtractsTheAddressesOfTheFirstNMessages()
     {
-        $generator = new AddressBookGenerator(2, array(
+        $generator = new AddressBookGenerator(2, new MessagesStub(array(
             new Message('1@example.com', null),
             new Message('2@example.com', null),
             new Message('3@example.com', null)
-        ));
+        )));
+    }
+}
+
+class MessagesStub implements Messages
+{
+    public function __construct(array $messages)
+    {
+        $this->messages = $messages;
+    }
+
+    public function getNext()
+    {
+        list ($key, $value) = each($this->messages);
+        return $value;
     }
 }

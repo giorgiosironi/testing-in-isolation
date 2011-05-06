154162f9ef89d83964993b6b1b64eeb9d11af1e6
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-05-06 13:55:28 +0200

    Extracted incomplete unit test for future PlainTextAddress implementation

---
+++ tests/PlainTextAddressTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class PlainTextAddressTest extends PHPUnit_Framework_TestCase
+{
+    public function testShowAnAddressWithNoLink()
+    {
+        // $address = new PlainTextAddress('isaac@example.com');
+        $this->markTestIncomplete();
+    }
+}

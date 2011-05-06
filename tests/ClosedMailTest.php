ece275f4e18c0091c2d27826703046233d66bcca
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-05-06 13:24:26 +0200

    Updated requirement: 'mailto:' link should be displayed

--- tests/ClosedMailTest.php
+++ tests/ClosedMailTest.php
@@ -2,12 +2,12 @@
 
 class ClosedMailTest extends PHPUnit_Framework_TestCase
 {
-    public function testShowAMailSubjectAndSenderAddress()
+    public function testShowAMailSubjectAndSenderAddressWithAMailtoLink()
     {
         $mail = new ClosedMail('Urgent', 'isaac@example.com');
         $expected = "<tr>\n"
                   . "<td class=\"subject\">Urgent</td>\n"
-                  . "<td class=\"address\">isaac@example.com</td>\n"
+                  . "<td class=\"address\"><a href=\"mailto:isaac@example.com\">isaac@example.com</a></td>\n"
                   . "</tr>";
         $this->assertEquals($expected, $mail->__toString());
     }

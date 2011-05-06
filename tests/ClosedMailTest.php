c1bdc2e93509753dcc92e26e000783129afc45c9
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-05-06 13:29:36 +0200

    Introduced two collaborators conforming to the same interface: MailtoAddress and NewMessageLinkAddress

--- tests/ClosedMailTest.php
+++ tests/ClosedMailTest.php
@@ -4,7 +4,7 @@ class ClosedMailTest extends PHPUnit_Framework_TestCase
 {
     public function testShowAMailSubjectAndSenderAddressWithAMailtoLink()
     {
-        $mail = new ClosedMail('Urgent', 'isaac@example.com');
+        $mail = new ClosedMail('Urgent', new MailtoAddress('isaac@example.com'));
         $expected = "<tr>\n"
                   . "<td class=\"subject\">Urgent</td>\n"
                   . "<td class=\"address\"><a href=\"mailto:isaac@example.com\">isaac@example.com</a></td>\n"
@@ -14,7 +14,7 @@ class ClosedMailTest extends PHPUnit_Framework_TestCase
 
     public function testShowAMailSubjectAndSenderAddressWithAnInApplicationLink()
     {
-        $mail = new ClosedMail('Urgent', 'isaac@example.com', false);
+        $mail = new ClosedMail('Urgent', new NewMessageLinkAddress('isaac@example.com'));
         $expected = "<tr>\n"
                   . "<td class=\"subject\">Urgent</td>\n"
                   . "<td class=\"address\"><a href=\"/new-mail?to=isaac@example.com\">isaac@example.com</a></td>\n"
